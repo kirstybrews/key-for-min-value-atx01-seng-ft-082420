833bd3af55e45341abe16e97cb25316015e3cbee
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-08-13 01:45:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if value < min_value 
       min_value = value 
-      key = final_key
+      final_key = key
     end
   end
   final_key 
